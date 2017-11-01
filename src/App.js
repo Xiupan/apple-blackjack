a3e9ed2ff65ac82aa3698128be5708dff0ae8adc
Alan Hong <xiupan@gmail.com>
2017-11-01 14:49:00 -0500

    Created deck array and shuffle function.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+let deckArray = ["A","A","A","A","K","K","K","K","Q","Q","Q","Q","J","J","J","J","10","10","10","10","9","9","9","9","8","8","8","8","7","7","7","7","6","6","6","6","5","5","5","5","4","4","4","4","3","3","3","3","2","2","2","2"]
+
+const shuffle = inputArray => {
+  let j, temp, i
+  for (i = inputArray.length - 1; i > 0; i--) {
+      j = Math.floor(Math.random() * (i + 1))
+      temp = inputArray[i];
+      inputArray[i] = inputArray[j];
+      inputArray[j] = temp;
+  }
+}
+
+shuffle(deckArray)
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {deckArray}
       </div>
     );
   }
