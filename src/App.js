934ff5c0516c96689353c468574b206d65b3a31c
Alan Hong <xiupan@gmail.com>
2017-11-01 15:42:46 -0500

    Hand value function created.

--- src/App.js
+++ src/App.js
@@ -16,15 +16,38 @@ const shuffle = inputArray => {
 shuffle(deckArray)
 let playerHand = []
 let dealerHand = []
+let playerHandValue = {
+  value: 0,
+  soft: "Nope"
+}
+let dealerHandValue = {
+  value: 0,
+  soft: "Nope"
+}
 
 const deal = (hand, deck) => {
   hand.push(deck.pop())
+}
+
+const calcHandValue = (hand, handObj) => {
+  for (let i = 0; i < hand.length; i++) {
+    if (hand[i] === "K" || hand[i] === "Q" || hand[i] === "J") {
+      handObj.value += 10
+    } else if (hand[i] === "A") {
+      handObj.value += 10
+      handObj.soft = "Yup"
+    } else {
+      handObj.value += parseInt(hand[i])
+    }
+  }
 }
 
 deal(playerHand, deckArray)
 deal(dealerHand, deckArray)
 deal(playerHand, deckArray)
 deal(dealerHand, deckArray)
+calcHandValue(playerHand, playerHandValue)
+calcHandValue(dealerHand, dealerHandValue)
 
 class App extends Component {
   render() {
@@ -38,9 +61,17 @@ class App extends Component {
           </div>
           <div>
             Player Hand: {playerHand}
+            <br/>
+            Player Hand Value: {playerHandValue.value}
+            <br/>
+            Soft? {playerHandValue.soft}
           </div>
           <div>
             Dealer Hand: {dealerHand}
+            <br/>
+            Dealer Hand Value: {dealerHandValue.value}
+            <br/>
+            Soft? {dealerHandValue.soft}
           </div>
         </div>
       </div>
